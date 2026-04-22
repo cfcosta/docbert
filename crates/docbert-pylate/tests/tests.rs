7a858d548300af4602263998fd3761754b1b222b
Cainã Costa <me@cfcosta.com>
2026-04-22 01:01:31 -0300

    chore: format fixes

--- crates/docbert-pylate/tests/tests.rs
+++ crates/docbert-pylate/tests/tests.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use candle_core::Device;
-use docbert_pylate::{ColBERT, hierarchical_pooling};
+use docbert_pylate::{hierarchical_pooling, ColBERT};
 
 fn load_model(repo_id: &str, device: Device) -> Result<ColBERT> {
     Ok(ColBERT::from(repo_id).with_device(device).try_into()?)
